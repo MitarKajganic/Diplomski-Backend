c6d5115b65c54b32202bbbcc202c016b09478e44
MitarKajganic <mkajganic9921rn@raf.rs>
2024-10-06 01:23:17 +0200

    Init

---
+++ src/main/java/com/mitar/dipl/DiplomskiProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.mitar.dipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomskiProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomskiProjekatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitar/dipl/DiplomskiProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitar.dipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomskiProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
