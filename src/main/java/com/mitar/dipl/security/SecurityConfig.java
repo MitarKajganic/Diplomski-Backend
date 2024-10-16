bdfc1483a9f930fc0231a79f160effd10ffe5c1f
MitarKajganic <mkajganic9921rn@raf.rs>
2024-10-16 04:29:45 +0200

    prebacene promenljive na env variable, podesen cors

--- src/main/java/com/mitar/dipl/security/SecurityConfig.java
+++ src/main/java/com/mitar/dipl/security/SecurityConfig.java
@@ -25,6 +25,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -42,8 +48,11 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, DispatcherServlet dispatcherServlet) throws Exception {
         http
+                // Enable CORS and configure CORS policy
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
                 // Disable CSRF as we are using JWT
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -96,5 +105,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8081")); // Update with your frontend URL
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
